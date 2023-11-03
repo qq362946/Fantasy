4c3c3bc36a6b31541dc1cdb95ddc6f266ba799bc
roubincode <liaoxiangning@taikr.com>
2023-11-03 15:56:52 +0800

    通信测试服务端项目加上命令行参数设置
    修改项目文件引用目录错误

--- examples/Communication-Exercises/Server/Hotfix/Model/MoveSyncComponent.cs
+++ examples/Communication-Exercises/Server/Hotfix/Model/MoveSyncComponent.cs
@@ -4,6 +4,20 @@ using Fantasy.DataStructure;
 using Fantasy.Helper;
 
 namespace BestGame;
+
+public class StartMoveEventHanlder : EventSystem<EventSystemStruct.StartMove>
+{
+    public override void Handler(EventSystemStruct.StartMove self)
+    {
+        var unit = self.unit;
+        MoveSyncComponent moveSyncComponent = unit.GetComponent<MoveSyncComponent>();
+
+        // 可以加BroadcastWithAoi，略过...
+
+        moveSyncComponent.AddMessage(unit.Id, self.moveInfo);
+    }
+}
+
 public class MoveSyncDestroySystem : DestroySystem<MoveSyncComponent>
 {
     protected override void Destroy(MoveSyncComponent self)
@@ -16,55 +30,23 @@ public class MoveSyncDestroySystem : DestroySystem<MoveSyncComponent>
     }
 }
 
-public class MoveSyncComponent : Entity 
+public class MoveSyncComponent : StateSync 
 {
     public M2C_MoveBroadcast Message = new M2C_MoveBroadcast();
-    public OneToManyList<long, MoveInfo> mDict = new OneToManyList<long, MoveInfo>();
-    public bool IsWait;
 
-    public void AddMessage(long unitId, MoveInfo moveInfo)
-    {
-        mDict.Add(unitId, moveInfo);
-
-        if (IsWait) return;
-        AddSync().Coroutine();
-    }
-
-    public async FTask AddSync()
-    {
-        IsWait = true;
-        long runtimeId = RuntimeId;
-
-        // 延迟15毫秒发送，让mDict收集数据
-        await TimerScheduler.Instance.Core.WaitAsync(15);
-        
-        // 防止异步的时候自己销毁了、下面逻辑执行的不对
-        if (runtimeId != RuntimeId) return;
-        
-        IsWait = false;
-        Send();
-    }
-
-    /// 是如何把玩家的移动状态广播给所有人的呢（有AOI就是附近所有人）？
-    /// ==》玩家附近只要有人状态有变化，就把这些变化的状态加到消息队列，每15毫秒的消息量发送一次给玩家
-    public void Send()
+    public override void Send()
     {
         Message.Moves.Clear();
 
-        /// 只是测试练习，还没有AOI组件
-        /// 所有玩家，有移动状态变化的位置信息列表
-        foreach (KeyValuePair<long, List<MoveInfo>> dic in mDict)
+        /// 所有玩家，有状态变化的数据列表
+        foreach (KeyValuePair<long, List<AProto>> dic in mDict)
         { 
-            foreach (MoveInfo info in dic.Value)
+            foreach (AProto info in dic.Value)
             {
-                Message.Moves.Add(info);
+                Message.Moves.Add((MoveInfo)info);
             }
         }
 
-        var unit = (Unit)Parent;
-        /// 向玩家发送所有的移动状态
-        MessageHelper.SendInnerRoute(unit.Scene,unit.SessionRuntimeId,
-            Message
-        );
+        SendMessage(Message);
     }
 }
