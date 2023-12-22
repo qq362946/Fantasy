fc63fe45915af3d63f7f61c6d38752b28b660084
qq362946 <362946@qq.com>
2023-12-22 19:44:52 +0800

    导表工具修复了选择增量导出会错误的清空文件夹的问题。

--- Tools/Src/Exporter/Excel/ExcelWorksheets.cs
+++ Tools/Src/Exporter/Excel/ExcelWorksheets.cs
@@ -1,32 +1,22 @@
+using Fantasy.Exporter;
 using OfficeOpenXml;
 
 namespace Exporter.Excel;
 
-public sealed class ExcelWorksheets
+public sealed class ExcelWorksheets(ExcelExporter excelExporter)
 {
-    private readonly ExcelExporter _excelExporter;
-    private readonly ExcelWorksheet _defaultExcelWorksheet;
-    public ExcelWorksheets(ExcelExporter excelExporter)
-    {
-        this._excelExporter = excelExporter;
-        _defaultExcelWorksheet = new ExcelPackage().Workbook.Worksheets.Add("VersionInfo");
-        _defaultExcelWorksheet.Cells["A1"].Value = "VersionInfo";
-    }
-    
     public bool TryGetValue(string worksheetName, out ExcelWorksheet excelWorksheet)
     {
-        if (_excelExporter.Worksheets.TryGetValue(worksheetName, out excelWorksheet))
+        if (excelExporter.Worksheets.TryGetValue(worksheetName, out excelWorksheet))
         {
             return true;
         }
 
-        if (!_excelExporter.VersionInfo.WorksheetNames.Contains(worksheetName))
+        if (!excelExporter.VersionInfo.WorksheetNames.Contains(worksheetName))
         {
-            return false;
+            Log.Info($"{worksheetName} is not exist!");
         }
         
-        excelWorksheet = _defaultExcelWorksheet;
-        return true;
-
+        return false;
     }
 }
