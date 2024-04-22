c561972d95a26fdec14f6a6c46f8c790d16a3358
qq362946 <362946@qq.com>
2024-04-22 15:06:47 +0800

    Fantasy ThreadScene版 1、增加了Scene线程功能、分为MainThread、MultiThread、ThreadPool三种类型。 2、具体可以查看Fantasy.Net/Config/SceneConfig.xlsx文件，里面有详细解释. 3、因为考虑线程Scene的线程安全问题、取消了框架内所有静态方法。 4、比如MessageHelper、Timer这些，现在统一在Scene下面、比如Scene.TimerComponent等。 5、优化了网络协议解包的效率问题,比上一代版本提升了大约1.5倍的效率，上一代因为每次申请内存过大所以耗时比较多。 6、去掉了上一代初始化框架必须要绑定配置表的问题，现在只需要再启动参数添加--c 配置表路径就可以了。比如:--c ../../../Config/Binary. 7、Fantasy.Unity去掉了Fantasy.UI功能，以后Fantasy.UI会以单独的包出现。 8、删除掉上一代版本的所有示例，后面会根据当前版本的特色来增加一些使用示例。 9、这次更新比较大，建议升级的慎重选择，大家可以切换Fantasy0.1分支找到上一代的版本。

--- Packages.Unity/Fantasy.Unity/Runtime/Fantasy.App/UI/FantasyScrollView/IScrollItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Fantasy
-{
-    public interface IScrollItem<in T>
-    {
-        void OnCreate(IFantasyScrollView scrollView);
-        void OnShowOrRefresh(int idx, T data);
-        void OnHide(int idx);
-    }
-}
