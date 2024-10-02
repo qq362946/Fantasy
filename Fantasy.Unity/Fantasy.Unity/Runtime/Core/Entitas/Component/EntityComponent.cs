c58a6fd6c54ac9f9328872d94e5c815ac5af4ed5
qq362946 <362946@qq.com>
2024-10-02 12:03:28 +0800

    修改StopUpdate方法为非泛型参数。

--- Fantasy.Unity/Fantasy.Unity/Runtime/Core/Entitas/Component/EntityComponent.cs
+++ Fantasy.Unity/Fantasy.Unity/Runtime/Core/Entitas/Component/EntityComponent.cs
@@ -359,9 +359,9 @@ namespace Fantasy.Entitas
         /// </summary>
         /// <param name="entity">实体对象</param>
         /// <typeparam name="T"></typeparam>
-        public void StopUpdate<T>(T entity) where T : Entity
+        public void StopUpdate(Entity entity)
         {
-            var type = typeof(T);
+            var type = entity.GetType();
             
             if (!_updateQueueDic.Remove(type,out var updateQueueInfo))
             {
